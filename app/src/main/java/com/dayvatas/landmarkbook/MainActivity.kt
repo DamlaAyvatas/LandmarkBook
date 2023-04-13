9939d45d4b6506bfb030d0ae7176e627e397a276
Damla Ayvatas <47346126+DamlaAyvatas@users.noreply.github.com>
2023-04-13 23:55:23 +0300

    Data added to LandmarkList

--- app/src/main/java/com/dayvatas/landmarkbook/MainActivity.kt
+++ app/src/main/java/com/dayvatas/landmarkbook/MainActivity.kt
@@ -6,10 +6,24 @@ import com.dayvatas.landmarkbook.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
+    private lateinit var landmarkList : ArrayList<Landmark>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        landmarkList = ArrayList<Landmark>()
+        //Data
+        val pisa = Landmark("Pisa Tower", "Italy", R.drawable.pisa)
+        val londonBridge = Landmark("London Bridge", "England", R.drawable.london)
+        val eiffel = Landmark("Eiffel Tower", "France", R.drawable.eiffel)
+        val colosseum = Landmark("Colosseum", "Italy", R.drawable.collesium)
+
+        landmarkList.add(pisa)
+        landmarkList.add(londonBridge)
+        landmarkList.add(eiffel)
+        landmarkList.add(colosseum)
+
     }
 }
